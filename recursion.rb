00a6a22b152099ef571864d1f8ff054100afde31
App Academy Student <email@email.com>
2022-02-16 13:56:30 -0500

    a few more solutions

--- recursion.rb
+++ recursion.rb
@@ -17,10 +17,40 @@
 # For example, range(1, 5) should return [1, 2, 3, 4]. If end < start, you can return an empty array.
 
 
-def rec_range(start, max) #2, 3
-  return start if start == max - 1  # s2: 2 == 2
-  rec_range(start + 1, max) # rec_range(2, 3)
+# def rec_range(start, max) #rec_range(1, 2)
+#   return [] if start >= max
+#   rec_range(start, max -1) << max - 1
+# end
+
+# def rec_range(start, max) #rec_range(1, 2)
+#   return [] if start >= max
+#   rec_range(start + 1, max).unshift(start)
+
+# p rec_range(1, 20)
+
+def exp_1(base, exp)
+  return 1 if exp <= 0
+  return base if exp == 1
+
+  base * exp_1(base, exp - 1) 
 end
+
+# p exp_1(2, 3)
+
+def exp_2(b, n) 
+  return 1 if n == 0
+  return b if n == 1
+
+  if n.even?
+    exp_2(b, n / 2) ** 2
+  else
+    b * (exp_2(b, (n - 1) / 2) ** 2)   
+  end
+end
+
+# p exp_2(2, 5)
+
+
 
 
 
